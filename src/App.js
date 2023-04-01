318656235b1c1940ee86608d85daf9b66770d335
=ahmedmehmoodcb <=ahmedmehmood.cb@gmail.com>
2023-04-01 14:54:54 +0500

    setup with autocomplete and search list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Autocomplete ,SearchList} from './components'
+
+const App = () => {
+  return (
+    <>    
+    <Autocomplete/>    
+    <SearchList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Autocomplete from "./Autocomplete/Autocomplete";
+import SearchList from "./List/SearchList";
+
+export {
+    Autocomplete,
+    SearchList,
+}
